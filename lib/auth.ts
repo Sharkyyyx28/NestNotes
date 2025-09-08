ffc1549b51c49d5a9e664524b1262cd1fd7c80ea
Aditya Verma <145123911+Sharkyyyx28@users.noreply.github.com>
2025-09-09 02:40:15 +0530

    Refactor: fix log issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,13 +2,13 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db/drizzle";
 import { schema } from "@/db/schema";
-import { EmailTemplate } from "../components/EmailTemplate";
 import { nextCookies } from "better-auth/next-js";
 import { Resend } from "resend";
-import { verification } from "@/auth-schema";
 import VerificationEmail from "@/components/emails/verification-email";
+import React from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
@@ -18,12 +18,13 @@ export const auth = betterAuth({
     schema,
   }),
   emailVerification: {
-    sendVerificationEmail: async ({ user, url, token }, request) => {
-      const { data, error } = await resend.emails.send({
+    sendVerificationEmail: async ({ user, url }) => {
+      // send verification email with Resend
+      await resend.emails.send({
         from: "Notes-app <website@resend.dev>",
         to: [user.email],
         subject: "Verify your email address",
-        react: VerificationEmail({
+        react: React.createElement(VerificationEmail, {
           userEmail: user.email,
           verificationUrl: url,
         }),
